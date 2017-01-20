b3057273b4b50bd58dfa609b12ae20fa3915ef28
allen shiu <allen.shiu@ufispace.com>
2017-01-20 16:45:55 +0800

    Initial Commit

---
+++ ShiuAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShiuAnimation
+//
+//  Created by AllenShiu on 2017/1/20.
+//  Copyright © 2017年 AllenShiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShiuAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShiuAnimation
+//
+//  Created by AllenShiu on 2017/1/20.
+//  Copyright © 2017年 AllenShiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
